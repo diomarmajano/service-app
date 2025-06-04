8777afae8b7e78475fa4c899bd653210f090ddaa
Diomar Majano <http.diomar@gmail.com>
2025-06-04 12:46:49 -0400

    implementacion del login del sistema

---
+++ resources/js/script.js
@@ -0,0 +1,28 @@
+const loginBtn = document.getElementById("loginButton");
+if (loginBtn) {
+    loginBtn.addEventListener("click", function() {
+        const credenciales = JSON.parse(localStorage.getItem("credenciales"));
+        var email = document.getElementById("serviceName").value;
+        var password = document.getElementById("servicePassword").value;
+
+        if (email === "" || password === "") {
+            alert("Por favor, complete todos los campos.");
+        } else if (credenciales && credenciales.email === email && credenciales.password === password) {
+            window.location.href = "/views/dashboard.html";
+        } else {
+            alert("Credenciales incorrectas.");
+        }
+    });
+}
+
+const registerBtn = document.getElementById("registerButton");
+if (registerBtn) {
+    registerBtn.addEventListener("click", function() { 
+        var credenciales = {
+            email: document.getElementById("registerName").value, 
+            password: document.getElementById("registerPassword").value
+        };
+        localStorage.setItem("credenciales", JSON.stringify(credenciales));
+        alert("Registro exitoso");
+    });
+}
